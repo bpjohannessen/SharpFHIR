4de77b8df0e05fd7e536ad51c79a6448b508c32b
Bjørn-Petter Johannessen <bpj@dips.no>
2016-07-14 16:48:41 +0200

    Added Enter as possibility for search

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfApplication1.Service; 
+using WpfApplication1.Service;
 
 namespace WpfApplication1
 {
@@ -21,25 +21,24 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly IFhirService m_fireService; 
-             
+        private readonly IFhirService m_fireService;
+
 
         public MainWindow()
         {
             m_fireService = new FhirWebService("http://spark.furore.com/fhir");
             InitializeComponent();
-            var names = m_fireService.GetPatients("Donald");
-            resultLabel.Content = "";
-            foreach(var a in names)
-            {
-                resultLabel.Content += a + "\r\n";
-                //
-            }
+            //var names = m_fireService.GetPatients("Donald");
+            //resultLabel.Content = "";
+            //foreach(var a in names)
+            //{
+            //    resultLabel.Content += a + "\r\n";
+            //}
         }
 
         private void nameTextbox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if(String.Equals(nameTextbox.Text, "Name to search for")) nameTextbox.Text = "";
+            if (String.Equals(nameTextbox.Text, "Name to search for")) nameTextbox.Text = "";
 
         }
 
@@ -50,13 +49,57 @@ namespace WpfApplication1
 
         private void nameTextbox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if(String.IsNullOrWhiteSpace(nameTextbox.Text))
+            if (String.IsNullOrWhiteSpace(nameTextbox.Text))
                 nameTextbox.Text = "Name to search for";
         }
         private void idTextbox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if(String.IsNullOrWhiteSpace(idTextbox.Text))
+            if (String.IsNullOrWhiteSpace(idTextbox.Text))
                 idTextbox.Text = "ID to search for";
+        }
+
+        private void nameSendButton_OnClicked(object sender, RoutedEventArgs e)
+        {
+            statusLabel.Content = "Searching..";
+
+            var names = m_fireService.GetPatients(nameTextbox.Text);
+            resultLabel.Content = "";
+            foreach (var a in names)
+            {
+                resultLabel.Content += a + "\r\n";
+            }
+
+            statusLabel.Content = "";
+
+        }
+
+        private void idSendButton_OnClicked(object sender, RoutedEventArgs e)
+        {
+
+            statusLabel.Content = "Searching..";
+            var names = m_fireService.GetPatients(nameTextbox.Text);
+            resultLabel.Content = "";
+            foreach (var a in names)
+            {
+                resultLabel.Content += a + "\r\n";
+            }
+            statusLabel.Content = "";
+        }
+
+        private void nameTextbox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                nameSendButton_OnClicked(sender, e);
+            }
+        }
+
+        private void idTextbox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                idSendButton_OnClicked(sender, e);
+            }
         }
     }
 
